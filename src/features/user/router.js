226578ff806d14fcd8b963dfd0914d873713ec3f
Dmitrii Mazurok <tmazurok@gmail.com>
2023-01-22 23:55:41 +0300

    add logging to delete user route

--- src/features/user/router.js
+++ src/features/user/router.js
@@ -1,6 +1,11 @@
 import { apiResponse } from "../../shared/api.js";
+import { logger } from "../../shared/logger.js";
 import { mapUserToResponse } from "./lib.js";
 import { getUsers } from "./repository.js";
+
+const ERROR = {
+  COULD_NOT_DELETE_USER: "could_not_delete_user",
+};
 
 export const showUsers = async (req, res) => {
   const users = await getUsers();
@@ -8,6 +13,21 @@ export const showUsers = async (req, res) => {
 };
 
 export const deleteUser = async (req, res) => {
-  await removeUserById(req.id);
+  const logOpts = {
+    requestId: crypto.randomUUID(),
+    user: { id: req.user?.id },
+    path: req.path,
+    deleting: { id: req.params.id },
+  };
+
+  try {
+    await removeUserById(req.id);
+  } catch (error) {
+    logger.error(error, logOpts);
+    return res.status(500).send(apiResponse(null, COULD_NOT_DELETE_USER));
+  }
+
+  logger.info("user deleted", logOpts);
+
   return res.sendStatus(200);
 };
